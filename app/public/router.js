e59c4f321f9968a7a6ae9452f7baa831f9ecec93
enigma01Z313 <f.ahmadyf94@gmail.com>
2022-09-29 14:45:27 +0330

    style/books pages

--- app/public/router.js
+++ app/public/router.js
@@ -1,16 +1,67 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const { books } = require("./staticDb");
+
+router.get("/books/", (req, res) => {
+  res.render("pages/books/index", { books });
+});
+
+router.get("/books/:bookId", (req, res) => {
+  const { bookId } = req.params;
+  const { title, chapters } = books.find((item) => item.id === bookId);
+
+  res.render("pages/books/archive", { bookId, title, chapters });
+});
+
+router.get("/books/:bookId/:chapterId", (req, res) => {
+  const { bookId, chapterId } = req.params;
+  let curIndex, prevChapter, nexChapter;
+
+  const { title: bookTitle, chapters } = books.find(
+    (item) => item.id === bookId
+  );
+  const { title, content } = chapters.find((chapter, i) => {
+    curIndex = i;
+    return chapter.id === chapterId;
+  });
+
+  nexChapter = curIndex < chapters.length - 1 ? curIndex + 1 : undefined;
+  prevChapter = curIndex > 0 ? curIndex - 1 : undefined;
+
+  nexChapter = typeof nexChapter !== typeof undefined && {
+    id: chapters[nexChapter].id,
+    title: chapters[nexChapter].title,
+  };
+  prevChapter = typeof prevChapter !== typeof undefined && {
+    id: chapters[prevChapter].id,
+    title: chapters[prevChapter].title,
+  };
+
+  res.render("pages/books/single", {
+    title,
+    content,
+    book: { id: bookId, title: bookTitle },
+    chapterNav: { next: nexChapter, prev: prevChapter },
+  });
+});
 
 router.get("/*", (req, res, next) => {
   const { url: requstedUrl } = req;
-  console.log(requstedUrl);
+  // console.log(requstedUrl);
 
   if (requstedUrl.includes("robots.txt")) return res.end("robots.txt");
 
   if (requstedUrl.includes("assets"))
-    return res.sendFile(path.join(__dirname, requstedUrl));
+    return res.sendFile(
+      path.join(
+        path.resolve("./"),
+        "app/public",
+        requstedUrl.match(/\assets(.*)/)[0]
+      )
+    );
 
+  //general pages handles
   if (requstedUrl[requstedUrl.length - 1] === "/")
     return res.render(`pages${requstedUrl}index`);
 
---
+++ app/public/staticDb.js
@@ -0,0 +1,40 @@
+module.exports = {
+  books: [
+    {
+      id: "the-regressed-demonlord-is-kind",
+      title: "The Regressed Demon Lord Is Kind",
+      chapters: [
+        {
+          id: "chapter-1",
+          title: "Chapter 1",
+          content: "page content of chapter 1",
+        },
+        {
+          id: "chapter-2",
+          title: "Chapter 2",
+          content: "page content of chapter 2",
+        },
+        {
+          id: "chapter-3",
+          title: "Chapter 3",
+          content: "page content of chapter 3",
+        },
+        {
+          id: "chapter-4",
+          title: "Chapter 4",
+          content: "page content of chapter 4",
+        },
+      ],
+    },
+    {
+      id: "superman",
+      title: "Superman",
+      chapters: [],
+    },
+    {
+      id: "batman",
+      title: "Batman",
+      chapters: [],
+    },
+  ],
+};
